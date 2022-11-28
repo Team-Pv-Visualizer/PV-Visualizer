2ecc3e8880931821a4b8c2bc62edaae227b50b20
William Lau <55828115+SendN00ts@users.noreply.github.com>
2022-11-28 19:18:33 +0100

    changes to code of website

---
+++ BackendxFrontend/pvv_website/assets/js/slideshow.js
@@ -0,0 +1,35 @@
+var slideIndex = 0;
+showSlides();
+
+function currentSlide(n) {
+    if (n > slides.length) {n = 1}
+        else if(n < 1) {n = slides.length}
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    
+    slides[n-1].style.display = "block";  
+    dots[n-1].className += " active";
+}
+
+function showSlides() {
+    var i;
+    slides = document.getElementsByClassName("mySlides");
+    dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 10000); // Change iframe every 10 seconds
+}
+
